758550df791019872c5fd7842bdce258878d0568
sauchin <sauchin@hotmail.com>
2014-07-19 23:29:18 +0800

    rails tutorial first_app
    rails tutorial first_app

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-first_app::Application.configure do
+rails.Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
