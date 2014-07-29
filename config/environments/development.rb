428bd5bbc40a9691acfc16c458c31ab4c9c93451
sauchin <sauchin@hotmail.com>
2014-07-29 19:45:27 +0800

    changed to firstapp in aplication.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-railsprojects::Application.configure do
+firstapp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
